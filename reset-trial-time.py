e6c539266d5ec1d9f1858c438ad71dc2ba49f727
kusholino <138784204+kusholino@users.noreply.github.com>
2023-07-07 12:13:28 +0200

    fixed issues

--- reset-trial-time.py
+++ reset-trial-time.py
@@ -1,15 +1,17 @@
 import subprocess
 from trashSite import createSite, rmSite, copyFile
 
+sitename = "sitename"
 with open(sitename, 'r') as file:
     site = file.read()
 
+time = "time"
 with open(time, 'r') as file:
     time = file.read()
 
 trash = 'test'
-
-if time <= 10:
+print(time)
+if time is False:
     createSite(trash)
     copyFile(site, trash)
     rmSite(trash)
